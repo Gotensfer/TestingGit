48cfe8e207b7b448da42b85945a4a1e59e40f23a
Gotensfer <gotensferxl@gmail.com>
2021-10-07 13:15:06 -0500

    second commitment

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -4,5 +4,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Finally back at intelIj goddamanit");
+        System.out.println("Well well, new code added");
     }
 }
