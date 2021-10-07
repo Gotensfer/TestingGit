0da19e4fbc7a197e0f5a80684fc733eb1cf6e4b5
Gotensfer <gotensferxl@gmail.com>
2021-10-07 12:50:05 -0500

    Initial commit - creation of repository

---
+++ src/clases/Main.java
@@ -0,0 +1,8 @@
+package clases;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Finally back at intelIj goddamanit");
+    }
+}
